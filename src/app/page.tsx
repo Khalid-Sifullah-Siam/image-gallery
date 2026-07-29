6c2c80f04a4467fdf5b630ffbf17b39fe496608e
Khalid-Sifullah-Siam <khalidsiam1754@gmail.com>
2026-07-29 20:48:40 +0600

    feat: enhance homepage layout and styling
    - Wrapped the main content in a centered, padded, and shadowed div
    - Changed the background color to a lighter shade
    - Adjusted the font size of the heading for better visual hierarchy
    - Updated the min-height to ensure the content is centered vertically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen">
-
-      <h1 className="text-6xl font-bold text-green-600">
-        Welcome to Image Gallery
-      </h1>
-
+    <main className="flex justify-center items-center min-h-screen bg-sky-200">
+      <div className="w-full max-w-screen-2xl mx-auto p-20 bg-slate-100 rounded-2xl shadow-lg">
+        <h1 className="text-5xl font-bold text-green-600">
+          Welcome to Image Gallery
+        </h1>
+      </div>
     </main>
   );
 }

1cdea4a500eb4e6339f4289245d20a33d30fee7c
Khalid-Sifullah-Siam <khalidsiam1754@gmail.com>
2026-09-10 00:11:43 +0600

    feat: update PhotoCard hover effects and next.config.ts

--- src/Components/PhotoCard.tsx/PhotoCard.tsx
+++ src/Components/PhotoCard.tsx/PhotoCard.tsx
@@ -1,10 +1,42 @@
+import Image from "next/image";
+import { BiSave } from "react-icons/bi";
 
 
 const PhotoCard = () => {
+    interface ImageInfo {
+        id: number
+        name: string
+        url: string
+        category: string
+        title: string
+        likes: number
+        shares: number
+    }
+
+    const image: ImageInfo = {
+        id: 1,
+        name: "Image 1",
+        url: "https://i.ibb.co.com/Cs5v91Jy/1-jpg.avif",
+        category: "Category 1",
+        title: "Title 1",
+        likes: 102,
+        shares: 50
+    }
     return (
-        <div>
-            photocard
-        </div>
+        <figure className="relative h-full group overflow-hidden">
+            <Image alt={image.title} src={image.url} width={400} height={400} className="w-full h-full object-cover hover:scale-105 transition-all duration-500" />
+
+            <figcaption className="absolute bottom-0 w-full p-2 text-white bg-slate-900/30 flex justify-between items-center gap-1.5 translate-y-full group-hover:translate-y-0 transition-transform duration-500 ease-in-out">
+                <div>
+                    <p className="font-semibold text-sm">{image.title}</p>
+                    <p className="text-xs opacity-90">{image.likes} like | {image.shares} shares</p>
+                </div>
+
+                <button>
+                    <BiSave size={25} />
+                </button>
+            </figcaption>
+        </figure>
     );
 };
 
--- src/Components/PhotosSection/PhotosSection.tsx
+++ src/Components/PhotosSection/PhotosSection.tsx
@@ -4,9 +4,6 @@ const PhotosSection = () => {
     return (
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             <PhotoCard />
-            <PhotoCard />
-            <PhotoCard />
-            <PhotoCard />
         </div>
     );
 };

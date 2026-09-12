1b4e189ca290810db7aa4669a459a0a1dd2841dc
Khalid-Sifullah-Siam <khalidsiam1754@gmail.com>
2026-09-13 02:06:59 +0600

    feat: update gallery with 6 unique ImgBB images

--- src/lib/db.tsx
+++ src/lib/db.tsx
@@ -3,50 +3,50 @@ import { ImageInfo } from "@/types/index.d";
 export const imagesData: ImageInfo[] = [
     {
         id: 1,
-        name: "Image 1",
+        name: "Mountain Sunset",
         url: "https://i.ibb.co.com/Cs5v91Jy/1-jpg.avif",
-        category: "Category 1",
-        likes: 102,
-        shares: 50
+        category: "Nature",
+        likes: 124,
+        shares: 45
     },
     {
         id: 2,
-        name: "Image 2",
-        url: "https://i.ibb.co.com/Cs5v91Jy/1-jpg.avif",
-        category: "Category 1",
-        likes: 102,
-        shares: 50
+        name: "Stage Concert",
+        url: "https://i.ibb.co.com/QFcxCyrg/singer-singing-silhouette-stage-singer-sing-silhouette-stage-135985450.webp",
+        category: "Music",
+        likes: 248,
+        shares: 89
     },
     {
         id: 3,
-        name: "Image 3",
-        url: "https://i.ibb.co.com/Cs5v91Jy/1-jpg.avif",
-        category: "Category 1",
-        likes: 102,
-        shares: 50
+        name: "Outdoor Scene",
+        url: "https://i.ibb.co.com/2157Vn53/image.jpg",
+        category: "Lifestyle",
+        likes: 180,
+        shares: 32
     },
     {
         id: 4,
-        name: "Image 4",
-        url: "https://i.ibb.co.com/Cs5v91Jy/1-jpg.avif",
-        category: "Category 1",
-        likes: 102,
-        shares: 50
+        name: "Urban Vibes",
+        url: "https://i.ibb.co.com/HL7k6bGr/images.jpg",
+        category: "Urban",
+        likes: 95,
+        shares: 20
     },
     {
         id: 5,
-        name: "Image 5",
-        url: "https://i.ibb.co.com/Cs5v91Jy/1-jpg.avif",
-        category: "Category 1",
-        likes: 102,
-        shares: 50
+        name: "Travel Insurance",
+        url: "https://i.ibb.co.com/SXLkKtcZ/Why-Do-You-Need-Travel-Insurance-When-Travelling-Abroad.jpg",
+        category: "Travel",
+        likes: 310,
+        shares: 115
     },
     {
         id: 6,
-        name: "Image 6",
-        url: "https://i.ibb.co.com/Cs5v91Jy/1-jpg.avif",
-        category: "Category 1",
-        likes: 102,
-        shares: 50
+        name: "Travel Lessons",
+        url: "https://i.ibb.co.com/JRPjh0nT/travel-lessons.jpg",
+        category: "Travel",
+        likes: 412,
+        shares: 160
     }
-]
+];
